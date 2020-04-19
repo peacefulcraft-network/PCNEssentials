a95ab26a9344c005a1b4db4d7b00199b166094bd
Parsonswy <parsonswy@gmail.com>
2020-04-18 20:41:21 -0400

    Actually fetch new config in reload command

--- src/net/peacefulcraft/rtp/commands/Reload.java
+++ src/net/peacefulcraft/rtp/commands/Reload.java
@@ -3,19 +3,29 @@ package net.peacefulcraft.rtp.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 
 import net.md_5.bungee.api.ChatColor;
 import net.peacefulcraft.rtp.PCNEssentials;
 
-public class Reload implements CommandExecutor{
+public class Reload implements CommandExecutor {
 
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if(sender.hasPermission("pcn.staff")) {
-            PCNEssentials.getPluginInstance().getCommand("rtp").setExecutor(
-                new RTP(PCNEssentials.getPluginInstance().getConfig())
-            );
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender.hasPermission("pcn.staff")) {
+            PCNEssentials.getPluginInstance().reloadConfig();
+            FileConfiguration c = PCNEssentials.getPluginInstance().getConfig();
 
-            PCNEssentials.getPluginInstance().getCommand("nv").setExecutor(new NightVision());
+            if (c.getBoolean("rtp.enabled")) {
+                PCNEssentials.getPluginInstance().getCommand("rtp").setExecutor(
+                    new RTP(PCNEssentials.getPluginInstance().getConfig())
+                );
+                PCNEssentials.getPluginInstance().logNotice("RTP: Enabled");
+            }
+
+            if (c.getBoolean("nv.enabled")) {
+                PCNEssentials.getPluginInstance().getCommand("nv").setExecutor(new NightVision());
+                PCNEssentials.getPluginInstance().logNotice("NV: Enabled");
+            }
 
             sender.sendMessage(ChatColor.GREEN + "[" + ChatColor.BLUE + "PCN" + ChatColor.GREEN + "]" + ChatColor.BLUE + "RTP Ranges Reloaded.");
         }
