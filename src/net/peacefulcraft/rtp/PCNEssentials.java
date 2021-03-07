9a7d7c8eecf09d41207c9010403e5c246c3ecd99
Parsonswy <parsonswy@gmail.com>
2021-03-06 21:26:00 -0500

    Stop bugging about updates when there are non

--- src/net/peacefulcraft/rtp/PCNEssentials.java
+++ src/net/peacefulcraft/rtp/PCNEssentials.java
@@ -24,7 +24,7 @@ import net.peacefulcraft.rtp.listeners.TurkeyListener;
 import net.peacefulcraft.rtp.scoreboard.ChallengeScoreboard;
 public class PCNEssentials extends JavaPlugin{
 
-	public static final String release = "0.0.2";
+	public static final String release = "0.0.3";
 
 	private static PCNEssentials p;
 		public static PCNEssentials getPluginInstance() { return p; }
