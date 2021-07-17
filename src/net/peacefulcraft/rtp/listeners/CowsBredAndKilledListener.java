387215cfa3b401ce8c4080f1be73de242012fd72
Parsonswy <parsonswy@gmail.com>
2021-07-16 22:48:25 -0400

    Respect the challenge time constraints

--- src/net/peacefulcraft/rtp/listeners/CowsBredAndKilledListener.java
+++ src/net/peacefulcraft/rtp/listeners/CowsBredAndKilledListener.java
@@ -9,11 +9,15 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityBreedEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import net.peacefulcraft.rtp.PCNEssentials;
+import net.peacefulcraft.rtp.configuration.Configuration;
 
 public class CowsBredAndKilledListener implements Listener {
 	
 	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void onCowBreed(EntityBreedEvent ev) {
+		Long curTime = System.currentTimeMillis();
+		if (curTime < Configuration.getCompetitionStartMS() || curTime > Configuration.getCompetitionEndMS()) { return; }
+
 		if (ev.getMother().getType() == EntityType.COW || ev.getMother().getType() == EntityType.MUSHROOM_COW) {
 			if (ev.getBreeder() instanceof Player) {
 				PCNEssentials.getChallengeScoreboard().incrimentScore((Player) ev.getBreeder());
@@ -23,6 +27,9 @@ public class CowsBredAndKilledListener implements Listener {
 
 	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void onCowKilled(EntityDamageByEntityEvent ev) {
+		Long curTime = System.currentTimeMillis();
+		if (curTime < Configuration.getCompetitionStartMS() || curTime > Configuration.getCompetitionEndMS()) { return; }
+
 		if (ev.getDamager() instanceof Player && (ev.getEntityType() == EntityType.COW || ev.getEntityType() == EntityType.MUSHROOM_COW)) {
 			Damageable cow = (Damageable) ev.getEntity();
 			if (cow.getHealth() <= ev.getFinalDamage()) {
