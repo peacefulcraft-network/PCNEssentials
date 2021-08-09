64ff6af3496c8ad6fdba4bd487261d08db6b9835
Parsonswy <parsonswy@gmail.com>
2021-08-09 08:42:32 -0400

    Cows bred challenge

--- src/net/peacefulcraft/rtp/listeners/CowsBredAndKilledListener.java
+++ src/net/peacefulcraft/rtp/listeners/CowsBredAndKilledListener.java
@@ -1,6 +1,8 @@
 package net.peacefulcraft.rtp.listeners;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Damageable;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -17,6 +19,19 @@ public class CowsBredAndKilledListener implements Listener {
 	public void onCowBreed(EntityBreedEvent ev) {
 		Long curTime = System.currentTimeMillis();
 		if (curTime < Configuration.getCompetitionStartMS() || curTime > Configuration.getCompetitionEndMS()) { return; }
+
+		int cowCount = 0;
+		for (Entity ent : ev.getEntity().getEyeLocation().getChunk().getEntities()) {
+			if (ent.getType() == EntityType.COW || ent.getType() == EntityType.MUSHROOM_COW) {
+				cowCount++;
+			}
+		}
+		if (cowCount > 40) {
+			// Kill the cow on the next tick
+			Bukkit.getScheduler().runTaskLater(PCNEssentials.getPluginInstance(), () -> {
+				ev.getEntity().damage(ev.getEntity().getHealth() + 1.0);
+			}, 1L);
+		}
 
 		if (ev.getMother().getType() == EntityType.COW || ev.getMother().getType() == EntityType.MUSHROOM_COW) {
 			if (ev.getBreeder() instanceof Player) {
